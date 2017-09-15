d961c105956b2864ae0c3b30b59cabbd84341cae
Akhil <akhilkrishna.r2016@vitstudent.ac.in>
2017-09-16 02:57:21 +0530

    Table inside table inside table.php

--- table.php
+++ table.php
@@ -1,3 +1,8 @@
+<!--dei i'm feeling very sleepy. I'll edit the rest tomorrow. i'll add these to the repo as of now-->
+
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,72 +10,393 @@
 </head>
 <body>
 <style>
-table{
+
+table.table1{
 	border-collapse: collapse;
 	width: 55%;
 	border:1px solid black;
 }
-th{
+table.table1 th{
 	border:1px solid black;
 	height:25px;
 }
-td{
+table.table1 td{
 	height:20px;
 	text-align: center;
 	border:1px solid black;
 }
-tr{
+table.table1 tr{
 	border-bottom: 1px solid black;
 }
-tr:nth-child(even){
-	background-color:#e0e0d1;
+
+table.table2 td{
+	border-left: 1px solid black;
+	text-align: center;
+	padding: 6px;
 }
+table.table2{
+	border:none;
+	border-collapse: collapse;
+	border-spacing: 8px 2px;
+}
+table.table2 tr{
+	text-align: center;
+}
+table.table2 td:first child{
+	border-left: none;
+
+
 </style>
-<div>
-<table style="float: left">
+
+
+<table class="table1">
+
 <tr>
 <th rowspan="2">Theory</th><th>Start</th><th>08:00</th><th>09:00</th><th>10:00</th><th>11:00</th><th>12:00</th><th></th><th rowspan="14">Lunch</th><th>14:00</th><th>15:00</th><th>16:00</th><th>17:00</th><th>18:00</th><th></th>
 </tr>
-<tr>
+<tr bgcolor="#e0e0d1">
 <th>End</th><th>08:50</th><th>09:50</th><th>10:50</th><th>11:50</th><th>12:50</th><th></th><th>14:50</th><th>15:50</th><th>16:50</th><th>17:50</th><th>18:50</th><th></th>
 </tr>
 <tr>
 <th rowspan="2">Lab</th><th>Start</th><th>08:00</th><th>-</th><th>10:00</th><th>-</th><th>11:50</th><th>-</th><th>14:00</th><th>-</th><th>16:00</th><th>-</th><th>17:50</th><th>-</th>
 </tr>
-<tr>
+<tr bgcolor="#e0e0d1">
 <th>End</th><th>-</th><th>09:40</th><th>-</th><th>11:40</th><th>-</th><th>13:30</th><th>-</th><th>15:40</th><th>-</th><th>17:40</th><th>-</th><th>19:30</th>
 </tr>
 <tr>
-<td rowspan="2" colspan="2"><b>MONDAY</b></td><td>A1</td><td>F1</td><td>D1</td><td>TB1</td><td>TG1</td><td></td><td>A2</td><td>F2</td><td>D2</td><td>TB2</td><td>TG2</td><td></td>
+	
+	<td rowspan="1" colspan="2"><b>MONDAY</b></td>
+	<td colspan="2"><a href="1.php?ls=l1" target="if1">
+		<table class="table2">
+			<tr>
+				<td>A1</td><td>F1</td>
+			</tr>
+			<tr>
+				<td>L1</td><td>L2</td>
+			</tr>
+		</table>
+	</a></td>
+	<td colspan="2"><a href="1.php?ls=l3" target="if1">
+		<table class="table2">
+			<tr>
+				<td>D1</td><td>TB1</td>
+			</tr>
+			<tr>
+				<td>L3</td><td>L4</td>
+			</tr>
+		</table>
+	</a></td>
+	
+	<td colspan="2"><a href="1.php?ls=l5" target="if1">
+		<table class="table2">
+			<tr>
+				<td>TG1</td><td></td>
+			</tr>
+			<tr>
+				<td>L5</td><td>L6</td>
+			</tr>
+		</table>
+	</a></td>
+
+	<td colspan="2"><a href="1.php?ls=l31" target="if1">
+		<table class="table2">
+			<tr>
+				<td>A2</td><td>F2</td>
+			</tr>
+			<tr>
+				<td>L31</td><td>L32</td>
+			</tr>
+		</table>
+	</a></td>
+	<td colspan="2"><a href="1.php?ls=l31" target="if1">
+		<table class="table2">
+			<tr>
+				<td>D2</td><td>TB2</td>
+			</tr>
+			<tr>
+				<td>L33</td><td>L34</td>
+			</tr>
+		</table>
+	</a></td>
+
+	<td colspan="2"><a href="1.php?ls=l33" target="if1">
+		<table class="table2">
+			<tr>
+				<td>TG2</td><td></td>
+			</tr>
+			<tr>
+				<td>L35</td><td>L36</td>
+			</tr>
+		</table>
+	</a></td>
+	
 </tr>
-<tr>
-<td>L1</td><td>L2</td><td>L3</td><td>L4</td><td>L5</td><td>L6</td><td>L31</td><td>L32</td><td>L33</td><td>L34</td><td>L35</td><td>L36</td>
+
+<tr bgcolor="#e0e0d1">
+	<td rowspan="1" colspan="2"><b>TUESDAY</b></td>
+	<td colspan="2"><a href="1.php?ls=l7" target="if1">
+		<table class="table2">
+			<tr>
+				<td>B1</td><td>G1</td>
+			</tr>
+			<tr>
+				<td>L7</td><td>L8</td>
+		</table>
+	</a></td>
+	
+	<td colspan="2"><a href="1.php?ls=l9" target="if1">
+		<table class="table2">
+			<tr>
+				<td>E1</td><td>TC1</td>
+			</tr>
+			<tr>
+				<td>L9</td><td>L10</td>
+			</tr>
+		</table>
+	</a></td>
+
+	<td colspan="2"><a href="1.php?ls=l11" target="if1">
+		<table class="table2">
+			<tr>
+				<td>TAA1</td><td></td>
+			</tr>
+			<tr>
+				<td>L11</td><td>L12</td>
+			</tr>
+		</table>
+	</a></td>
+	
+	<td colspan="2"><a href="1.php?ls=l37" target="if1">
+		<table class="table2">
+			<tr>
+				<td>B2</td><td>G2</td>
+			</tr>
+			<tr>
+				<td>L37</td><td>L38</td>
+			</tr>
+		</table>
+	</a></td>
+	
+	<td colspan="2"><a href="1.php?ls=l39" target="if1">
+		<table class="table2">
+			<tr>
+				<td>E2</td><td>TC2</td>
+			</tr>
+			<tr>
+				<td>L39</td><td>L40</td>
+			</tr>
+		</table>
+	</a></td>
+
+	<td colspan="2"><a href="1.php?ls=l41" target="if1">
+		<table class="table2">
+			<tr>
+				<td>TAA2</td><td></td>
+			</tr>
+			<tr>
+				<td>L41</td><td>L42</td>
+			</tr>
+		</table>
+	</a></td>
 </tr>
+
 <tr>
-<td rowspan="2" colspan="2"><b>TUEDAY</b></td><td>B1</td><td>G1</td><td>E1</td><td>TC1</td><td>TAA1</td><td></td><td>B2</td><td>G2</td><td>E2</td><td>TC2</td><td>TAA2</td><td></td>
+	<td rowspan="1" colspan="2"><b>WEDNESDAY</b></td>
+	<td colspan="2"><a href="1.php?ls=l13" target="if1">
+		<table class="table2">
+			<tr>
+				<td>C1</td><td>A1</td>
+			</tr>
+			<tr>
+				<td>L13</td><td>L14</td>
+			</tr>
+		</table>
+	</a></td>
+	
+	<td>
+		<table class="table2">
+			<tr>
+				<td>F1</td>
+			</tr>
+			<tr>
+				<td>-</td>
+			</tr>
+		</table>
+	</td>
+	<td rowspan="1" colspan="3" align="center"><b>Extramural</b></td>
+	<td colspan="2"><a href="1.php?ls=l43" target="if1">
+		<table class="table2">
+			<tr>
+				<td>C2</td><td>A2</td>
+			</tr>
+			<tr>
+				<td>L43</td><td>L44</td>
+			</tr>
+		</table>
+	</a></td>
+	
+	<td colspan="2"><a href="1.php?ls=l45" target="if1">
+		<table class="table2">
+			<tr>
+				<td>F2</td><td>TD2</td>
+			</tr>
+			<tr>
+				<td>L45</td><td>L46</td>
+			</tr>
+		</table>
+	</a></td>
+	
+	<td colspan="2"><a href="1.php?ls=l47" target="if1">
+		<table class="table2">
+			<tr>
+				<td>TBB2</td><td></td>
+			</tr>
+			<tr>
+				<td>L47</td><td>L48</td>
+			</tr>
+		</table>
+	</a></td>
 </tr>
-<tr>
-<td>L7</td><td>L8</td><td>L9</td><td>L10</td><td>L11</td><td>L12</td><td>L37</td><td>L38</td><td>L39</td><td>L40</td><td>L41</td><td>L42</td>
+
+<tr bgcolor="#e0e0d1">
+	<td rowspan="1" colspan="2"><b>THURDAY</b></td>
+	<td colspan="2"><a href="1.php?ls=l19" target="if1">
+		<table class="table2">
+			<tr>
+				<td>D1</td><td>B1</td>
+			</tr>
+			<tr>
+				<td>L19</td><td>L20</td>
+			</tr>
+		</table>
+	</a></td>
+	
+	<td colspan="2"><a href="1.php?ls=l21" target="if1">
+		<table class="table2">
+			<tr>
+				<td>G1</td><td>TE1</td>
+			</tr>
+			<tr>
+				<td>L21</td><td>L22</td>
+			</tr>
+		</table>
+	</a></td>
+
+	<td colspan="2"><a href="1.php?ls=l23" target="if1">
+		<table class="table2">
+			<tr>
+				<td>TCC1</td><td></td>
+			</tr>
+			<tr>
+				<td>L23</td><td>L24</td>
+			</tr>
+		</table>
+	</a></td>
+	
+	<td colspan="2"><a href="1.php?ls=l49" target="if1">
+		<table class="table2">
+			<tr>
+				<td>D2</td><td>B2</td>
+			</tr>
+			<tr>
+				<td>L49</td><td>L50</td>
+			</tr>
+		</table>
+	</a></td>
+	
+	<td colspan="2"><a href="1.php?ls=l51" target="if1">
+		<table class="table2">
+			<tr>
+				<td>G2</td><td>TE2</td>
+			</tr>
+			<tr>
+				<td>L51</td><td>L52</td>
+			</tr>
+		</table>
+	</a></td>
+	
+	<td colspan="2"><a href="1.php?ls=l53" target="if1">
+		<table class="table2">
+			<tr>
+				<td>TCC2</td><td></td>
+			</tr>
+			<tr>
+				<td>L53</td><td>L54</td>
+			</tr>
+		</table>
+	</a></td>
 </tr>
+
 <tr>
-<td rowspan="2" colspan="2"><b>WEDNESDAY</b></td><td>C1</td><td>A1</td><td>F1</td><td rowspan="2" colspan="3" align="center"><b>Extramural</b></td><td>C2</td><td>A2</td><td>F2</td><td>TD2</td><td>TBB2</td><td></td>
-</tr>
-<tr>
-<td>L13</td><td>L14</td><td>-</td><td>L43</td><td>L44</td><td>L45</td><td>L46</td><td>L47</td><td>L48</td>
-</tr>
-<tr>
-<td rowspan="2" colspan="2"><b>THURDAY</b></td><td>D1</td><td>B1</td><td>G1</td><td>TE1</td><td>TCC1</td><td></td><td>D2</td><td>B2</td><td>G2</td><td>TE2</td><td>TCC2</td><td></td>
-</tr>
-<tr>
-<td>L19</td><td>L20</td><td>L21</td><td>L22</td><td>L23</td><td>L24</td><td>L49</td><td>L50</td><td>L51</td><td>L52</td><td>L53</td><td>L54</td>
-</tr>
-<tr>
-<td rowspan="2" colspan="2"><b>FRIDAY</b></td><td>E1</td><td>C1</td><td>TA1</td><td>TF1</td><td>TD1</td><td></td><td>E2</td><td>C2</td><td>TA2</td><td>TF2</td><td>TDD2</td><td></td>
-</tr>
-<tr>
-<td>L25</td><td>L26</td><td>L27</td><td>L28</td><td>L29</td><td>L30</td><td>L55</td><td>L56</td><td>L57</td><td>L58</td><td>L59</td><td>L60</td>
+<td rowspan="1" colspan="2"><b>FRIDAY</b></td>
+	<td colspan="2"><a href="1.php?ls=l25" target="if1">
+		<table class="table2">
+			<tr>
+				<td>E1</td><td>C1</td>
+			</tr>
+			<tr>
+				<td>L25</td><td>L26</td>
+			</tr>
+		</table>
+	</a></td>
+	
+	<td colspan="2"><a href="1.php?ls=l27" target="if1">
+		<table class="table2">
+			<tr>
+				<td>TA1</td><td>TF1</td>
+			</tr>
+			<tr>
+				<td>L27</td><td>L28</td>
+			</tr>
+		</table>
+	</a></td>
+	
+	<td colspan="2"><a href="1.php?ls=l29" target="if1">
+		<table class="table2">
+			<tr>
+				<td>TD1</td><td></td>
+			</tr>
+			<tr>
+				<td>L29</td><td>L30</td>
+			</tr>
+		</table>
+	</a></td>
+	
+	<td colspan="2"><a href="1.php?ls=l55" target="if1">
+		<table class="table2">
+			<tr>
+				<td>E2</td><td>C2</td>
+			</tr>
+			<tr>
+				<td>L55</td><td>L56</td>
+			</tr>
+		</table>
+	</a></td>
+
+	<td colspan="2"><a href="1.php?ls=l57" target="if1">
+		<table class="table2">
+			<tr>
+				<td>TA2</td><td>TF2</td>
+			</tr>
+			<tr>
+				<td>L57</td><td>L58</td>
+			</tr>
+		</table>
+	</a></td>
+	
+	<td colspan="2"><a href="1.php?ls=l59" target="if1">
+		<table class="table2">
+			<tr>
+				<td>TDD2</td><td></td>
+			</tr>
+			<tr>
+				<td>L59</td><td>L60</td>
+			</tr>
+		</table>
+	</a></td>
+
 </tr>
 </table>
-</div>
+
 </body>
 </html>
