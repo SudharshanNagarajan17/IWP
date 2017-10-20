901c37b221a856dd2fda687e4a327229cf82150f
SudharshanNagarajan <sudharsh_n@yahoo.co.in>
2017-10-20 18:59:54 +0530

    insertion working

--- fac2.php
+++ fac2.php
@@ -13,16 +13,28 @@ if($e<=$d)
 include "link.php";	
 
 $conn=openCon();
+$k=0;
 $t="SELECT * FROM workshop";
 $a=mysqli_query($conn,$t);
 while($row=mysqli_fetch_array($a))
 {
 	if(strcmp($row['no'],$b)==0 && strcmp($row['dat'],$c)==0)
+	{
+		$k=1;
 		echo "<script>alert('Workshop taking place');	window.open(window.history.back(),'_self');</script>";
+	}
 }
-$conn=openCon();
-$p="INSERT INTO workshop(name,no,dat,tfr,tto,det) VALUES('w','t','r','d','a','c')";
-$a=mysqli_query($conn,$p);
-if($a)
-	echo "<script>alert('Workshop details added');	window.open('facwk.php','_self');</script>";
+$a=(string)$_POST['t1'];
+$b=(string)$_POST['s1'];
+$c=(string)$_POST['d1'];
+$d=(string)$_POST['tt1'];
+$e=(string)$_POST['tt2'];
+$f=(string)$_POST['t2'];
+if($k==0)
+{
+	$p="INSERT INTO workshop(name,no,dat,tfr,tto,det) VALUES('$a','$b','$c','$d','$e','$f')";
+	$a=mysqli_query($conn,$p);
+	if($a)
+		echo "<script>alert('Workshop details added');	window.open('facwk.php','_self');</script>";
+}
 ?>
